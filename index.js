e2828836f1a3e65074d76bbfb86d90e02ca53a59
Christian Lopez <clopez@MacBook-Pro-de-Desarollo-4.local>
2019-08-30 22:34:05 -0500

    Log

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const http = require('http')
 const app = express()
 
+app.use((req, res, next) => {
+  console.log(req)
+  next()
+})
 app.get('/status', (req, res) => {
   res.status(200).json({ message: 'OK' })
 })
